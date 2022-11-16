7b2da36e23f2639f2c002400b06c0d18f4399035
asrorc1k <asrornortojiev@gmail.com>
2022-11-16 11:59:58 +0300

    03

--- get_email.py
+++ get_email.py
@@ -9,3 +9,11 @@ def get_email(data:dict) -> list:
     Returns:
         list: users email
     """
+    answer = []
+    for i in range(20):
+        answer.append(data['results'][i]['email'])
+    return answer
+f = open('randomuser_data.json','r')
+f = f.read()
+data = json.loads(f)
+print(get_email(data))
